2fa91481d92104600cbb7f3d44834b0791880229
chinoad <46451027+chinoad@users.noreply.github.com>
2019-03-28 16:45:26 -0400

    Added a function
    Added a function so that the dices are thrown after a click

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+$(document).click(function(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var image1 = $("img")[0];
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-image1.setAttribute("src", randomImageSource);
+  var randomImageSource = "images/" + randomDiceImage;
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var image1 = $("img")[0];
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  image1.setAttribute("src", randomImageSource);
 
-var image2 = $("img")[1];
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-image2.setAttribute("src", randomImageSource2);
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 > randomNumber2) {
-$("h1").text( "Player 1 Win!" );
-} else if (randomNumber1 < randomNumber2) {
-$("h1").text( "Player 2 Wins!" );
-} else {
-$("h1").text("Draw!")
-}
+  var image2 = $("img")[1];
+
+  image2.setAttribute("src", randomImageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+  $("h1").text( "Player 1 Win!" );
+  } else if (randomNumber1 < randomNumber2) {
+  $("h1").text( "Player 2 Wins!" );
+  } else {
+  $("h1").text("Draw!")
+  }
+
+
+  })
