c67849686737035cc374ea851103755e4e581e5f
chinoad <46451027+chinoad@users.noreply.github.com>
2019-03-28 16:30:39 -0400

    Updated with jQuery

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = $("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
@@ -12,14 +12,14 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-var image2 = document.querySelectorAll("img")[1];
+var image2 = $("img")[1];
 
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Win!"
+$("h1").text( "Player 1 Win!" );
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!"
+$("h1").text( "Player 2 Wins!" );
 } else {
-  document.querySelector("h1").innerHTML = "Draw!"
+$("h1").text("Draw!")
 }
